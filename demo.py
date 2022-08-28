4541f926e5b4952435a7c1139cd1874f8a274c6a
Fengyi Li <fengyi.li18@imperial.ac.uk>
2022-08-28 21:02:34 +0800

    For validation part

--- demo.py
+++ demo.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append('core')
 
 import argparse
@@ -11,14 +12,18 @@ from raft_stereo import RAFTStereo
 from utils.utils import InputPadder
 from PIL import Image
 from matplotlib import pyplot as plt
-
+import re
+import os
 
 DEVICE = 'cuda'
+root = r"vol/bitbucket/fl4718/Utils/rectified_rendered_data"
+
 
 def load_image(imfile):
     img = np.array(Image.open(imfile)).astype(np.uint8)
     img = torch.from_numpy(img).permute(2, 0, 1).float()
     return img[None].to(DEVICE)
+
 
 def demo(args):
     model = torch.nn.DataParallel(RAFTStereo(args), device_ids=[0])
@@ -32,8 +37,22 @@ def demo(args):
     output_directory.mkdir(exist_ok=True)
 
     with torch.no_grad():
-        left_images = sorted(glob.glob(args.left_imgs, recursive=True))
-        right_images = sorted(glob.glob(args.right_imgs, recursive=True))
+        left_images = []
+        right_images = []
+        if args.rendered:
+            with open("splits/new_rendered/test_files.txt", "r") as tf:
+                test_lists = tf.readlines()
+                for line in test_lists:
+                    view = line.split()[0]
+                    index = line.split()[1]
+                    if view == "Left":
+                        left_images.append(os.path.join(root, "Left", f"rgb_{index}.PNG"))
+                    else:
+                        right_images.append(os.path.join(root, "Right", f"rgb_{index}.PNG"))
+        else:
+            left_images = sorted(glob.glob(args.left_imgs, recursive=True))
+            right_images = sorted(glob.glob(args.right_imgs, recursive=True))
+
         print(f"Found {len(left_images)} images. Saving files to {output_directory}/")
 
         for (imfile1, imfile2) in tqdm(list(zip(left_images, right_images))):
@@ -47,29 +66,50 @@ def demo(args):
             file_stem = imfile1.split('/')[-2]
             if args.save_numpy:
                 np.save(output_directory / f"{file_stem}.npy", flow_up.cpu().numpy().squeeze())
-            plt.imsave(output_directory / f"{file_stem}.png", -flow_up.cpu().numpy().squeeze(), cmap='jet')
+
+            folder = args.folder
+            if os.path.exists(os.path.join(output_directory, folder)):
+                pass
+            else:
+                os.mkdir(os.path.join(output_directory, folder))
+
+            if os.path.exists(os.path.join(output_directory, folder, file_stem)):
+                pass
+            else:
+                os.mkdir(os.path.join(output_directory, folder, file_stem))
+
+            plt.imsave(os.path.join(output_directory, folder, file_stem, imfile1.split("/")[-1]),
+                       -flow_up.cpu().numpy().squeeze(), cmap='jet', vmin=0)
+            # plt.imsave(output_directory / f"{file_stem}.png", -flow_up.cpu().numpy().squeeze(), cmap='jet', vmax=255)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--folder', help="restore checkpoint", default=r"20k_val")
+    parser.add_argument('--output_directory', help="directory to save output", default="demo_output/rendered")
+    parser.add_argument("--rendered", action="store_true")
     parser.add_argument('--restore_ckpt', help="restore checkpoint", required=True)
     parser.add_argument('--save_numpy', action='store_true', help='save output as numpy arrays')
-    parser.add_argument('-l', '--left_imgs', help="path to all first (left) frames", default="datasets/Middlebury/MiddEval3/testH/*/im0.png")
-    parser.add_argument('-r', '--right_imgs', help="path to all second (right) frames", default="datasets/Middlebury/MiddEval3/testH/*/im1.png")
-    parser.add_argument('--output_directory', help="directory to save output", default="demo_output")
+    parser.add_argument('-l', '--left_imgs', help="path to all first (left) frames",
+                        default="datasets/Middlebury/MiddEval3/testH/*/im0.png")
+    parser.add_argument('-r', '--right_imgs', help="path to all second (right) frames",
+                        default="datasets/Middlebury/MiddEval3/testH/*/im1.png")
     parser.add_argument('--mixed_precision', action='store_true', help='use mixed precision')
     parser.add_argument('--valid_iters', type=int, default=32, help='number of flow-field updates during forward pass')
 
     # Architecture choices
-    parser.add_argument('--hidden_dims', nargs='+', type=int, default=[128]*3, help="hidden state and context dimensions")
-    parser.add_argument('--corr_implementation', choices=["reg", "alt", "reg_cuda", "alt_cuda"], default="reg", help="correlation volume implementation")
-    parser.add_argument('--shared_backbone', action='store_true', help="use a single backbone for the context and feature encoders")
+    parser.add_argument('--hidden_dims', nargs='+', type=int, default=[128] * 3,
+                        help="hidden state and context dimensions")
+    parser.add_argument('--corr_implementation', choices=["reg", "alt", "reg_cuda", "alt_cuda"], default="reg",
+                        help="correlation volume implementation")
+    parser.add_argument('--shared_backbone', action='store_true',
+                        help="use a single backbone for the context and feature encoders")
     parser.add_argument('--corr_levels', type=int, default=4, help="number of levels in the correlation pyramid")
     parser.add_argument('--corr_radius', type=int, default=4, help="width of the correlation pyramid")
-    parser.add_argument('--n_downsample', type=int, default=2, help="resolution of the disparity field (1/2^K)")
+    parser.add_argument('--n_downsample', type=int, default=3, help="resolution of the disparity field (1/2^K)")
     parser.add_argument('--slow_fast_gru', action='store_true', help="iterate the low-res GRUs more frequently")
     parser.add_argument('--n_gru_layers', type=int, default=3, help="number of hidden GRU levels")
-    
+
     args = parser.parse_args()
 
     demo(args)
